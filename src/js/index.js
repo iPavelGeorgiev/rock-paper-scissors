2c4aa3ff835fd7802e39919f88d4e8762571ecf8
Pavel Georgiev <ipavelgeorgiev@outlook.com>
2022-11-25 21:42:10 +0200

    Add difficulty switch component

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,31 @@ import {getDifficulty, getScore, setScore, animateCSS} from './utilities.js';
 // Selectors & Variables
 const scoreboardSel = document.querySelector('.scoreboard');
 const mainSel = document.querySelector('#main');
+const difficultySwitchSel = document.querySelector('.difficulty-switch');
+const choicePanelSel = () => document.querySelector('#choice-panel');
+
+// Functions
+const switchDifficulty = (e) => {
+  if (e.target.tagName !== 'INPUT') return;
+
+  const difficulty = document.querySelector('.difficulty-switch-input:checked').value;
+  localStorage.setItem('difficulty', difficulty);
+
+  const score = getScore(difficulty);
+  setScore(score);
+  animateCSS('.score-total', 'zoomIn');
+
+  const logo = createLogo(difficulty);
+  const choicePanel = createChoicePanel(difficulty);
+
+  updateModalContent(difficulty);
+  document.querySelector('#result-panel')?.remove();
+  document.querySelector('#logo').replaceWith(logo);
+  choicePanelSel().replaceWith(choicePanel);
+};
+
+// Event Listeners
+difficultySwitchSel.addEventListener('click', switchDifficulty);
 
 // Init
 (function init() {
@@ -15,6 +40,7 @@ const mainSel = document.querySelector('#main');
   const logo = createLogo(difficulty);
   const choicePanel = createChoicePanel(difficulty);
 
+  document.querySelector(`input#difficulty-${difficulty}`).checked = true;
   updateModalContent(difficulty);
   scoreboardSel.prepend(logo);
   mainSel.append(choicePanel);
