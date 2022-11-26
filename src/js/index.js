2329f6c41c37ed1a37a3ef02269d72e4cef3ff45
Pavel Georgiev <ipavelgeorgiev@outlook.com>
2022-11-26 12:59:25 +0200

    Add play round logic

--- src/js/index.js
+++ src/js/index.js
@@ -7,12 +7,50 @@ const mainSel = document.querySelector('#main');
 const difficultySwitchSel = document.querySelector('.difficulty-switch');
 const choicePanelSel = () => document.querySelector('#choice-panel');
 
+const choiceBeats = {
+  rock: ['scissors', 'lizard'],
+  paper: ['rock', 'spock'],
+  scissors: ['paper', 'lizard'],
+  lizard: ['spock', 'paper'],
+  spock: ['scissors', 'rock'],
+};
+
 // Functions
 const getHouseChoice = () => {
   const difficulty = getDifficulty();
   const randomInt = Math.floor(Math.random() * choices[difficulty].length);
 
   return choices[difficulty][randomInt];
+};
+
+const playRound = (e) => {
+  if (e.target.tagName !== 'BUTTON') return;
+
+  const difficulty = getDifficulty();
+  const score = getScore(difficulty);
+  const playerChoice = e.target.dataset.choice;
+  const houseChoice = getHouseChoice();
+  let resultPanel;
+  let newScore;
+
+  if (playerChoice === houseChoice) {
+    newScore = score;
+    resultPanel = createResultPanel(playerChoice, houseChoice, 'none');
+  } else if (choiceBeats[playerChoice].includes(houseChoice)) {
+    newScore = score + 1;
+    resultPanel = createResultPanel(playerChoice, houseChoice, playerChoice);
+  } else {
+    newScore = score > 0 ? score - 1 : score;
+    resultPanel = createResultPanel(playerChoice, houseChoice, houseChoice);
+  }
+
+  choicePanelSel().classList.add('hidden');
+  setTimeout(() => {
+    localStorage.setItem(`score-${difficulty}`, newScore);
+    setScore(newScore);
+  }, 2000);
+
+  mainSel.append(resultPanel);
 };
 
 const switchDifficulty = (e) => {
@@ -27,6 +65,7 @@ const switchDifficulty = (e) => {
 
   const logo = createLogo(difficulty);
   const choicePanel = createChoicePanel(difficulty);
+  choicePanel.addEventListener('click', playRound);
 
   updateModalContent(difficulty);
   document.querySelector('#result-panel')?.remove();
@@ -46,6 +85,7 @@ difficultySwitchSel.addEventListener('click', switchDifficulty);
 
   const logo = createLogo(difficulty);
   const choicePanel = createChoicePanel(difficulty);
+  choicePanel.addEventListener('click', playRound);
 
   document.querySelector(`input#difficulty-${difficulty}`).checked = true;
   updateModalContent(difficulty);
